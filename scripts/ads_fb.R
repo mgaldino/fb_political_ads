291498122cb42d3113163a183e6a7c8bc1e0b6a4
mgaldino <mcz.fea@gmail.com>
2020-08-25 13:31:44 -0300

    Adicionei como criptografar o token.

--- scripts/ads_fb.R
+++ scripts/ads_fb.R
@@ -1,8 +1,36 @@
 library(httr)
 library(tidyverse)
 library(lubridate)
+library(sodium) # para encriptar token
+library(getPass) # pra digitar no prompt
 
-token_face_mkt <- "EAAvjGQVxB9YBAE4JvMHCVUZCXMza0OtEeALsSRuid07JApnaqsF7yLOAxzmOm7ZAKNFbk7XPrcR6HKGx81bgb6C5hTMQZACHZAIxv0wkgnHUNSc9nOwexx7SOZAkdxFxYCdA4b5vbLgXztYxEV58ZBxokyMlGsvea6dmDMEqBhavdn1FI1RvsRZADzuvBZCJsKZCkco99FwmBNdWhCflYZAnZBQ"
+# # função pra digitar senha no prompt
+# senha_segura <- function() {
+#   senha <- getPass::getPass("Digite sua senha: ")
+#   senha_check <- getPass::getPass("Digite sua senha novamente: ")
+#   if(senha != senha_check) {
+#     print("Erro: senha digitada não corresponde à primeira senha.")
+#   }
+#   my_passkey <- sha256(charToRaw(senha))
+# }
+# 
+# # função que criptografa o token
+# criptografa_token <- function() {
+#   passkey <- senha_segura()
+#   token <- getPass::getPass("Digite seu token: ")
+#   token.raw <- serialize(token, NULL)
+#   ciphertext <- data_encrypt(token.raw, key = passkey)
+# }
+# 
+# # criptogragando o token
+# ciphertext <- criptografa_token()
+# 
+# # salvando token criptografado no R
+# saveRDS(ciphertext, file="token_criptografado.rds")
+
+# carrega token criptografado e descriptografa
+ciphertext <- readRDS("token_criptografado.rds")
+token_descriptografado <- unserialize(data_decrypt(ciphertext, key = senha_segura()))
 
 
 #link to fb api
@@ -24,19 +52,18 @@ search_fields=c("ad_creation_time", "ad_delivery_start_time", "ad_delivery_stop_
 #get the data from the first 'page' of data the api provides
 page_one_response <- GET(my_link,
                          path = "/ads_archive",
-                         query = list(access_token = token_face_mkt,
+                         query = list(access_token = token_descriptografado,
                                       limit=5000,
                                       ad_active_status="ALL",
                                       search_terms="''",
                                       fields=search_fields,
                                       ad_reached_countries="BR"))
+
 page_one_content <- content(page_one_response)
 
 x <- tibble(data=page_one_content$data)
 df_imp <- x %>% 
   unnest_wider(data) 
-
-glimpse(df_imp)
 
 #get the link refering to the next page
 next_link <- page_one_content$paging$`next`
@@ -60,11 +87,13 @@ while(length(next_link)>0) {
 glimpse(df_imp)
 # código adaptado de https://rpubs.com/zoowalk/FB_EP2019/
 
+# cria formato data
 df_imp <- df_imp %>%
   mutate(ad_creation_time1 = gsub("T", " ", ad_creation_time),
          ad_creation_time1 = gsub("+0000", "", ad_creation_time1), 
-         ad_creation_time2 = ymd_hms(ad_creation_time1))
-
+         ad_creation_time1 = ymd_hms(ad_creation_time1))
+# análise
+# ver anúncios que mencionam Bolsonaro ou Lula
 df_imp %>%
   mutate(bol_bolsonaro  = str_detect(tolower(ad_creative_body), "bolsonaro"),
          bol_Lula  = str_detect(tolower(ad_creative_body), "lula")) %>%
@@ -72,8 +101,8 @@ df_imp %>%
   summarise(n())
 
 df_imp %>%
-  summarise(max(ad_creation_time2),
-            min(ad_creation_time2))
+  summarise(max(ad_creation_time1),
+            min(ad_creation_time1))
 
 setwd("/home/mgaldino/Documentos/")
 saveRDS(df_imp, "ads_03_ago.rds")
